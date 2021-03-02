83d36b3c32ff586730cd2839c286a6f3ed68e5fa
Mihir Prajapati <68345194+Mihir-Prajapati@users.noreply.github.com>
2021-03-02 19:20:10 +0530

    Initial Commit for 4Anime site web project

---
+++ alert.js
@@ -0,0 +1,13 @@
+//alert('Welcome to 4Anime.\nHere you will find best quality anime.')
+var postx;
+function ChangeId(imgId){
+    console.log(imgId);
+    postx = imgId.id;
+    console.log(postx);
+    //Changepost(postx);
+}
+
+function Changepost(postx){
+    var postx;
+    console.log(postx);
+}
